deea20a9b4167877dd176d404df61840b2874340
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-21 10:12:51 +0300

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,11 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        <ProjectItem name='Reciplease' about='A recipe tracking app' technologies='Bootstrap Css'><span>Rails</span></ProjectItem>
+        <ProjectItem name='Kibbles N Bitz' about='Tinder for dogs' technologies='React'><span>Redux</span></ProjectItem>
+        <ProjectItem name='Alienwares' about='Etsy for Aliens' technologies='Rails'><span>React</span><span>Redux</span></ProjectItem>
+      </div>
     </div>
   );
 }
