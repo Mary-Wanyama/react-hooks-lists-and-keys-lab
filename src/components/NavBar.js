04c954b455b9e01ad71217d18c31caaeb42c4be2
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-27 03:32:46 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ function NavBar() {
 
   return <nav>
     
-    <a href={links[0]}>{links[0]}</a>
-    <a href={links[1]}>{links[1]}</a>
-    <a href={links[2]}>{links[2]}</a>
+    <a href="#home">{links[0]}</a>
+    <a href="#about">{links[1]}</a>
+    <a href="#projects">{links[2]}</a>
     
 
   </nav>;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,26 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+
+function ProjectItem({ name, about, technologies}) {
+
+
+
   return (
     <div className="project-item">
+      
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        <span>{technologies}</span>
-      </div>
+        <span>{technologies[0]}</span>
+        <span>{technologies[1]}</span>
+        <span>{technologies[2]}</span>
+        </div>
+      
+      
     </div>
   );
 }
+
+
 
 export default ProjectItem;
